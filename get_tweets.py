8d10b7dc0d02f1edc5a58c11201b632e3413304b
lplinkl <lplinkl00@gmail.com>
2022-07-07 13:15:37 +0800

    updated scraping

--- get_tweets.py
+++ get_tweets.py
@@ -18,25 +18,18 @@ def twitter_setup():
         print("error during authentication")
     return api
 
+words=["anus","arse", "arsehole", "ass", "ass-hat","ass-jabber","ass-pirate","assbag", "assbandit","assbanger","assbite", "assclown","asscock", "asscracker", "asses", "assface", "assfuck", "assfucker", "assgoblin","asshat", "asshead","asshole","asshopper", "assjacker", "asslick", "asslicker", "assmonkey","assmunch", "assmuncher", "assnigger","asspirate","assshit", "assshole","asssucker", "asswad", "asswipe", "axwound", "bampot","bastard","beaner","beartrap", "bitch", "bitchass", "bitches","bitchtits", "bitchy","blow job","blowjob","bollocks","bollox","boner","brotherfucker","bullshit","bumblefuck","butt plug","butt-pirate", "buttfucka","buttfucker","camel toe", "carpetmuncher", "chesticle","chinc", "chink","choad","chode", "clit", "clitface", "clitfuck", "clitweasel", "clusterfuck", "cock", "cockass", "cockbite", "cockburger", "cockface", "cockfucker", "cockhead", "cockjockey", "cockknoker", "cockmaster", "cockmongler", "cockmongruel","cockmonkey", "cockmuncher", "cocknose", "cocknugget", "cockshit", "cocksmith", "cocksmoke","cocksmoker","cocksniffer","cocksucker", "cockwaffle", "coochie","coochy", "coon", "cooter", "cracker","cum", "cumbubble", "cumdumpster", "cumguzzler", "cumjockey", "cumslut", "cumtart","cunnie","cunnilingus", "cunt", "cuntass", "cuntface","cunthole","cuntlicker", "cuntrag", "cuntslut","dago", "damn", "deggo","dick","dick-sneeze", "dickbag", "dickbeaters", "dickface", "dickfuck", "dickfucker", "dickhead", "dickhole", "dickjuice", "dickmilk", "dickmonger","dicks","dickslap", "dicksucker", "dicksucking", "dicktickler", "dickwad", "dickweasel","dickweed", "dickwod", "dike", "dildo", "dipshit", "docking", "doochbag", "dookie", "douche", "douche-fag", "douchebag", "douchewaffle", "dumass", "dumb ass", "dumbass", "dumbfuck", "dumbshit", "dumshit", "dyke", "fag", "fagbag", "fagfucker", "faggit", "faggot", "faggotcock", "fagnut", "fagtard", "fatass", "fellatio", "feltch", "flamer", "fuck", "fuckass", "fuckbag", "fuckboy", "fuckbrain","fuckbutt","fuckbutter","fucked", "fucker", "fuckersucker", "fuckface", "fuckhead","fuckhole", "fuckin", "fucking", "fucknose", "fucknut", "fucknutt","fuckoff","fucks","fuckstick", "fucktard", "fucktart", "fuckup", "fuckwad", "fuckwit","fuckwitt", "fudgepacker", "gay", "gayass", "gaybob", "gaydo", "gayfuck", "gayfuckist", "gaylord","gaytard","gaywad", "goddamn", "goddamnit", "gooch", "gook", "goopchute", "gringo", "guido", "handjob", "hard on", "heeb", "hell","ho", "hoe","homo", "homodumbshit", "honkey", "humping", "jackass", "jagoff", "jap", "jerk off","jerkass", "jigaboo", "jizz","jungle bunny","junglebunny", "kike", "kooch", "kootch", "kraut", "kunt", "kyke", "lameass","lardass","lesbian", "lesbo", "lezzie", "masturbate", "mcfagget", "mick","minge", "mothafucka", "mothafuckin\'", "motherfucker","motherfucking", "muff", "muffdiver","munging","nazi", "negro", "nigaboo", "nigga", "nigger","niggerish", "niggers", "niglet", "nignog","nut sack", "nutsack", "paki", "panooch", "pecker","peckerhead", "penis", "penisbanger","penisfucker", "penispuffer", "piss", "pissed", "pissed off", "pissflaps", "polesmoker","pollock", "poon","poonani", "poonany", "poontang", "porch monkey", "porchmonkey", "prick","punanny", "punta", "pussies", "pussy", "pussylicking", "puto","queef", "queer","queerbait", "queerhole", "renob", "rimjob", "ruski", "sand nigger", "sandnigger", "schlong", "scrote", "shit", "shitass", "shitbag", "shitbagger", "shitbrains", "shitbreath", "shitcanned", "shitcunt", "shitdick", "shitface", "shitfaced","shithead", "shithole", "shithouse", "shitspitter", "shitstain", "shitter", "shittiest", "shitting", "shitty", "shiz", "shiznit", "skank", "skeet", "skullfuck", "slut", "slutbag", "smeg", "snatch", "spic", "spick", "splooge", "spook","suckass", "tard", "testicle", "thundercunt", "tit", "titfuck", "tits", "tittyfuck", "twat", "twatlips", "twats", "twatwaffle", "twit","uglyfuck", "unclefucker","va-j-j", "vag", "vagina", "vajayjay","vjayjay", "wank", "wankjob", "wetback", "whore", "whorebag", "whoreface", "wop" ]
 
 extractor = twitter_setup()
-
-def get_hashtag_tweets(api, hashtag):
+def get_tweets(api, words):
     tweets = []
-    for content in tweepy.Cursor(api.search_tweets, q=hashtag).items(): #screen_name is the twitter handle, not the display name i.e the one with "@SomeUser"
-        tweets.append(content)
+    for w in words:
+        for content in tweepy.Cursor(api.search_tweets, q=w, lang="en").items(100): #screen_name is the twitter handle, not the display name i.e the one with "@SomeUser"
+            tweets.append(content)
     
     return tweets
 
-def keyword_tweets(api, keyword, number_of_tweets):
-    new_keyword = keyword + " -filter:retweets"
-    tweets = []
-    for status in tweepy.Cursor(api.search_tweets, q=new_keyword, lang="en").items(number_of_tweets):
-        tweets.append(status)
-    
-    return tweets
-
-alltweets = get_hashtag_tweets(extractor, 'Cyberbullying')
+alltweets = get_tweets(extractor, words)
 print("number of tweets extracted:", len(alltweets))
 
 print("5 recent tweets:\n")
